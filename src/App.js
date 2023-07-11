289905422698b1a9ddaafb1df3c229c1d8137d70
Luzivanlt <gokuhdvn3@gmail.com>
2023-07-11 17:24:57 +0700

    fix responsive

--- src/App.js
+++ src/App.js
@@ -24,6 +24,36 @@ function App() {
         Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
         `,
       },
+      {
+        header: "Interests",
+        content: `
+        Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+        `,
+      },
+      {
+        header: "Interests",
+        content: `
+        Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+        `,
+      },
+      {
+        header: "Interests",
+        content: `
+        Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+        `,
+      },
+      {
+        header: "Interests",
+        content: `
+        Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+        `,
+      },
+      {
+        header: "Interests",
+        content: `
+        Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+        `,
+      },
     ],
   };
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,14 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSquareTwitter, faSquareFacebook, faSquareInstagram, faSquareGithub } from '@fortawesome/free-brands-svg-icons';
+import { faSquareTwitter, faSquareFacebook, faSquareInstagram, faSquareGithub, faAccusoft } from '@fortawesome/free-brands-svg-icons';
 
 export default function Footer() {
 
     return (
         <div className="footing">
-            <FontAwesomeIcon icon={faSquareTwitter} style={{ color: '#918E9B' }}/>
-            <FontAwesomeIcon icon={faSquareFacebook} style={{ color: '#918E9B' }}/>
-            <FontAwesomeIcon icon={faSquareInstagram} style={{ color: '#918E9B' }}/>
-            <FontAwesomeIcon icon={faSquareGithub} style={{ color: '#918E9B' }}/>
+            <FontAwesomeIcon icon={faSquareTwitter} style={{ color: '#918E9B' }} />
+            <FontAwesomeIcon icon={faSquareFacebook} style={{ color: '#918E9B' }} />
+            <FontAwesomeIcon icon={faSquareInstagram} style={{ color: '#918E9B' }} />
+            <FontAwesomeIcon icon={faSquareGithub} style={{ color: 'pink' }} />
         </div>
     );
 }
